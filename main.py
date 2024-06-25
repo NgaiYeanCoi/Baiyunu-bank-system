d3eec4a7494901134756f75482dbdeb2c1337671
NgaiYeanCoi <2232047183@stayrabbit.top>
2024-06-25 21:24:25 +0800

    Add some description

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python3
+#-*- coding: UTF-8 -*-
+
 ' Baiyunu bank system '
+
 __author__ = 'NgaiYeancoi','canyie'
 
-
-
-
+import tkinter as tk
 def main():
     pass
 if __name__ == '__main__':
