9687c32cd01ed5c4d0f3dbd6ea5a84e610fb6b5d
NgaiYeanCoi <2232047183@stayrabbit.top>
2024-06-26 09:00:21 +0800

    Update some GUI

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 __author__ = 'NgaiYeancoi','canyie'
 
 import tkinter as tk
+from tkinter import messagebox
 ## GUI的部分
 root= tk.Tk() #建立Tkinter视窗
 root.title('Baiyunu Bank System') #设置窗口标题
@@ -17,8 +18,20 @@ height = 600 #高
 left = int((window_width-width)/2) #计算左坐标
 top = int((window_height-height)/2) #计算上坐标 以保证在中间显示
 root.geometry(f'{width}x{height}+{left}+{top}')  #定义视窗大小
+label1 = tk.Label(root,text='白云学院银行管理系统',font=('宋体',50,'bold','italic')) #建立标签
+label1.pack(side='top')
 
 
+
+def createaccount():
+    tk.messagebox.askokcancel('test','test')
+    pass
+btn1 = tk.Button(root, # 建立Button按钮
+                 text="开户",
+                 width=20,
+                 height=5,
+                 bd=2,command=createaccount)
+btn1.pack(side='left')  # btn1加入视窗
 root.mainloop()  # 进入消息循环
 
 def main():
